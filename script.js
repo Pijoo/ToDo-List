d45786bd26e65e56fb17adc664268d0114b79e74
Bartosz Surzyn <surzyn_b@interia.pl>
2018-07-24 19:59:14 +0200

    a little bit closer :)

--- script.js
+++ script.js
@@ -100,7 +100,18 @@ var view = {
 		todosUl.innerHTML = '';
 		for (var i = 0; i < todoList.todos.length; i++) {
 			var todoLi = document.createElement('li');
+			var todo = todoList.todos[i];
+			var todoTextWithCompletion = '';
+
+			if(todo.complited === true) {
+				todoTextWithCompletion = '(x) ' + todo.todoText;
+			} else {
+                todoTextWithCompletion = '( ) ' + todo.todoText;
+			}
+
+			todoLi.textContent = todoTextWithCompletion;
 			todosUl.appendChild(todoLi);
 		}
 	}
 }
+
