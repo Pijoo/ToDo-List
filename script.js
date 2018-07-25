745a129655f4c3571fbb866b7ca5186503e7e812
Bartosz Surzyn <surzyn_b@interia.pl>
2018-07-25 21:23:07 +0200

    displayTodos deleted

--- script.js
+++ script.js
@@ -2,39 +2,21 @@
 
 var todoList = {
 	todos: [],
-	displayTodos: function {
-		if (this.todos.length === 0) {
-			console.log('Your Todo List is empty');
-		} else {
-			console.log('My Todos: ');
-			for (var i=0; i < this.todos.length; i++) {
-				if (this.todos[i].completed === true) {
-					console.log('(x)', this.todos[i].todoText);
-				} else {
-					console.log('( )', this.todos[i].todoText);
-				}
-			}
-		}
-	},
 	addTodo: function(todoText) {
 		this.todos.push({
 			todoText: todoText;
 			completed: false
 		});
-		this.displayTodos();
 	},
 	changeTodos: function(position, todoText) {
 		this.todos[position].todoText = todoText;
-		this.displayTodos();
 	},
 	deleteTodos: function(position) {
 		this.todos.splice(position, 1);
-		this.displayTodos();
 	},
 	toggleCompleted: function(position) {
 		var todo = this.todos[position];
 		todo.completed = !todo.complited;
-		this.displayTodos();
 	}
 
 	toggleAll: function() {
@@ -57,14 +39,10 @@ var todoList = {
 					this.todos[i].completed = true;
 				}
 			}
-		this.displayTodos();
 	}
 };
 
 var handlers = {
-	displayTodos: function() {
-		todoList.displayTodos();
-	},
 	addTodo: function() {
 		var addTodoTextInput = document.getElementById('addTodoTextInput') {
 			todoList.addTodo(addTodoTextInput.value);
@@ -118,5 +96,5 @@ var view = {
 			todosUl.appendChild(todoLi);
 		}
 	}
-}
+};
 
